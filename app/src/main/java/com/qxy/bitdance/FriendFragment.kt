e641d9b4132ab7bdd75bd83bdde55c5280b9302d
hdh25490 <13896922098@163.com>
2022-07-30 13:26:42 +0800

    榜单布局编写

--- app/src/main/java/com/qxy/bitdance/FriendFragment.kt
+++ app/src/main/java/com/qxy/bitdance/FriendFragment.kt
@@ -27,6 +27,7 @@ class FriendFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
