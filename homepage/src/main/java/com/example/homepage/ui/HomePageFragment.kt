fde8caa04d70f37910c77e005613615ce1161aa9
Wu Dewei <wuleekey@qq.com>
2022-08-07 19:29:33 +0800

    优化homepage

--- homepage/src/main/java/com/example/homepage/ui/HomePageFragment.kt
+++ homepage/src/main/java/com/example/homepage/ui/HomePageFragment.kt
@@ -4,7 +4,6 @@ import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,9 +11,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.VideoView
-import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.viewpager2.widget.ViewPager2
 import com.example.homepage.R
 import com.google.android.material.tabs.TabLayout
